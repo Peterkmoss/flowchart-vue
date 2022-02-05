e6c501c1fae377f997d68d517c960321e162efb6
Peter K. Moss <peter@moss.dk>
2022-02-05 20:52:32 +0100

    add logic for connecting to top points

--- src/utils/Point/TopPoint.js
+++ src/utils/Point/TopPoint.js
@@ -26,58 +26,46 @@ export class TopPoint extends Point {
 
     const direction = this._getDirection();
     switch (direction) {
-      case 'd':
-      case 'rd': {
-        if (this.connectStart.x > this.fromPointBox.l) {
-          acc.push({ x: this.fromPointBox.l, y: this.connectEnd.y });
-          acc.push({ x: this.fromPointBox.l, y: this.middle.y });
-          acc.push({ x: this.connectStart.x, y: this.middle.y });
-        } else {
-          acc.push({ x: this.connectStart.x, y: this.connectEnd.y });
-        }
+      case 'l':
+        acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
+        return
+      case 'lu':
+      case 'ru':
+      case 'u':
+        acc.push({ x: this.connectEnd.x, y: this.middle.y });
+        acc.push({ x: this.connectStart.x, y: this.middle.y });
         return;
-      }
-      case 'ld': {
-        if (this.pointBox.b > this.connectEnd.y) {
-          acc.push({ x: this.connectEnd.x, y: this.pointBox.b });
-          acc.push({ x: this.connectStart.x, y: this.pointBox.b });
-        } else {
-          if (this.connectStart.x < this.fromPointBox.r) {
-            acc.push({ x: this.connectEnd.x, y: this.connectEnd.y });
-            acc.push({ x: this.fromPointBox.r, y: this.connectEnd.y });
-            acc.push({ x: this.fromPointBox.r, y: this.connectStart.y });
+      case 'ld':
+        if (this.pointBox.l < this.fromPointBox.r) {
+          if (this.connectStart.y > this.fromPointBox.t) {
+            acc.push({ x: this.fromPointBox.l, y: this.connectEnd.y });
+            acc.push({ x: this.fromPointBox.l, y: this.fromPointBox.t });
+            acc.push({ x: this.connectStart.x, y: this.fromPointBox.t });
           } else {
-            acc.push({ x: this.connectEnd.x, y: this.connectEnd.y });
-            acc.push({ x: this.connectStart.x, y: this.connectEnd.y });
+            acc.push({ x: this.fromPointBox.l, y: this.connectEnd.y });
+            acc.push({ x: this.fromPointBox.l, y: this.connectStart.y });
           }
+        } else {
+          acc.push({ x: this.middle.x, y: this.connectEnd.y });
+          acc.push({ x: this.middle.x, y: this.connectStart.y });
         }
         return;
-      }
-      case 'r': {
+      case 'r':
         acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
         return;
-      }
-      case 'u':
-      case 'ru': {
-        if (this.pointBox.r > this.connectEnd.x) {
-          acc.push({ x: this.pointBox.r, y: this.connectEnd.y });
-          acc.push({ x: this.pointBox.r, y: this.connectStart.y });
+      case 'rd':
+        if (this.pointBox.r > this.fromPointBox.l) {
+          acc.push({ x: this.pointBox.l, y: this.connectEnd.y });
+          acc.push({ x: this.pointBox.l, y: this.connectStart.y });
         } else {
-          acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
+          acc.push({ x: this.middle.x, y: this.connectEnd.y });
+          acc.push({ x: this.middle.x, y: this.connectStart.y });
         }
         return;
-      }
-      case 'l':
-      case 'lu': {
-        if (this.pointBox.t < this.connectEnd.y) {
-          acc.push({ x: this.connectEnd.x, y: this.pointBox.b });
-          acc.push({ x: this.connectStart.x, y: this.pointBox.b });
-        } else {
-          acc.push({ x: this.connectEnd.x, y: Math.min(this.pointBox.t, this.middle.y) });
-          acc.push({ x: this.connectStart.x, y: Math.min(this.pointBox.t, this.middle.y) });
-        }
+      case 'd':
+        acc.push({ x: this.pointBox.l, y: this.connectEnd.y });
+        acc.push({ x: this.pointBox.l, y: this.connectStart.y });
         return;
-      }
     }
   }
 
@@ -91,51 +79,48 @@ export class TopPoint extends Point {
     const direction = this._getDirection();
     switch (direction) {
       case 'l':
-      case 'lu': {
+        acc.push({ x: this.connectEnd.x, y: this.fromPointBox.t });
+        acc.push({ x: this.connectStart.x, y: this.fromPointBox.t });
+        return;
+      case 'ld':
+        if (this.pointBox.t > this.fromPointBox.t) {
+          acc.push({ x: this.connectEnd.x, y: this.fromPointBox.t });
+          acc.push({ x: this.connectStart.x, y: this.fromPointBox.t });
+        } else {
+          if (this.pointBox.l < this.connectEnd.x) {
+            acc.push({ x: this.pointBox.l, y: this.connectEnd.y });
+            acc.push({ x: this.pointBox.l, y: this.connectStart.y });
+          } else {
+            acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
+          }
+        }
+        return;
+      case 'lu':
         if (this.pointBox.t < this.fromPointBox.b) {
-          if (this.pointBox.l < this.connectEnd.x) {
-            acc.push({ x: this.pointBox.l, y: this.connectEnd.y });
-            acc.push({ x: this.pointBox.l, y: this.pointBox.b });
-            acc.push({ x: this.pointBox.r, y: this.pointBox.b });
-          } else {
-            acc.push({ x: this.connectEnd.x, y: this.pointBox.b });
-            acc.push({ x: this.connectStart.x, y: this.pointBox.b });
-          }
+        acc.push({ x: this.connectEnd.x, y: this.fromPointBox.t });
+        acc.push({ x: this.connectStart.x, y: this.fromPointBox.t });
         } else {
           acc.push({ x: this.connectEnd.x, y: this.middle.y });
           acc.push({ x: this.connectStart.x, y: this.middle.y });
         }
         return;
-      }
-      case 'ld': {
-        if (this.pointBox.b > this.fromPointBox.t) {
-          if (this.pointBox.l < this.connectEnd.x) {
-            acc.push({ x: this.pointBox.l, y: this.connectEnd.y });
-            acc.push({ x: this.pointBox.l, y: this.pointBox.t });
-            acc.push({ x: this.pointBox.r, y: this.pointBox.t });
-          } else {
-            acc.push({ x: this.connectEnd.x, y: this.pointBox.t });
-            acc.push({ x: this.connectStart.x, y: this.pointBox.t });
-          }
-        } else {
-          acc.push({ x: this.connectEnd.x, y: this.middle.y });
-          acc.push({ x: this.connectStart.x, y: this.middle.y });
-        }
-        return;
-      }
       case 'r':
+      case 'ru':
+        acc.push({ x: this.connectStart.x, y: this.connectEnd.y });
+        return;
+      case 'd':
       case 'rd':
+        if (this.pointBox.r > this.connectEnd.x) {
+          acc.push({ x: this.pointBox.l, y: this.connectEnd.y });
+          acc.push({ x: this.pointBox.l, y: this.connectStart.y });
+        } else {
+          acc.push({ x: Math.max(this.pointBox.r, this.middle.x), y: this.connectEnd.y });
+          acc.push({ x: Math.max(this.pointBox.r, this.middle.x), y: this.connectStart.y });
+        }
+        return;
       case 'u':
-      case 'ru': {
-        acc.push({ x: this.middle.x, y: this.connectEnd.y });
-        acc.push({ x: this.middle.x, y: this.connectStart.y });
+        acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
         return;
-      }
-      case 'd': {
-        acc.push({ x: this.connectEnd.x, y: this.middle.y });
-        acc.push({ x: this.connectStart.x, y: this.middle.y });
-        return;
-      }
     }
   }
 
@@ -148,62 +133,57 @@ export class TopPoint extends Point {
 
     const direction = this._getDirection();
     switch (direction) {
-      case 'l': {
-        acc.push({ x: this.connectEnd.x, y: this.pointBox.t });
-        acc.push({ x: this.connectStart.x, y: this.pointBox.t });
+      case 'l':
+        acc.push({ x: this.connectStart.x, y: this.connectEnd.y });
         return;
-      }
-      case 'ld': {
-        if (this.pointBox.b > this.connectEnd.y) {
-          acc.push({ x: this.connectEnd.x, y: this.pointBox.b });
-          acc.push({ x: this.connectStart.x, y: this.pointBox.b });
-        } else {
-          acc.push({ x: this.connectStart.x, y: this.connectEnd.y });
-        }
-        return;
-      }
-      case 'lu': {
-        if (this.connectEnd.y > this.pointBox.t) {
-          acc.push({ x: this.connectEnd.x, y: this.pointBox.t });
-          acc.push({ x: this.pointBox.r, y: this.pointBox.t });
-          acc.push({ x: this.pointBox.r, y: this.connectStart.y });
-        } else {
-          acc.push({ x: this.connectStart.x, y: this.connectEnd.y });
-        }
-        return;
-      }
-      case 'r':
-      case 'rd': {
-        if (this.connectStart.y > this.fromPointBox.t) {
-          acc.push({ x: this.connectEnd.x, y: this.fromPointBox.t });
-          acc.push({ x: this.middle.x, y: this.fromPointBox.t });
-          acc.push({ x: this.middle.x, y: this.connectStart.y });
+      case 'ld':
+        if (this.pointBox.l < this.connectEnd.x) {
+          if (this.pointBox.b > this.connectEnd.y) {
+            acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
+          } else {
+            acc.push({ x: this.pointBox.r, y: this.connectEnd.y });
+            acc.push({ x: this.pointBox.r, y: this.connectStart.y });
+          }
         } else {
           acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
         }
         return;
-      }
+      case 'lu':
       case 'u':
-      case 'ru': {
-        if (this.connectStart.y > this.fromPointBox.b) {
-          if (this.connectEnd.x < this.pointBox.r) {
+        acc.push({ x: this.connectStart.x, y: this.connectEnd.y });
+        return;
+      case 'r':
+      case 'ru':
+        if (this.pointBox.t < this.fromPointBox.b) {
+          if (this.connectStart.x > this.fromPointBox.l) {
+            acc.push({ x: this.connectEnd.x, y: this.fromPointBox.t });
+            acc.push({ x: this.fromPointBox.l, y: this.fromPointBox.t });
+            acc.push({ x: this.fromPointBox.l, y: this.connectStart.y });
+          } else {
+            acc.push({ x: this.connectEnd.x, y: this.fromPointBox.t });
+            acc.push({ x: this.connectStart.x, y: this.fromPointBox.t });
+          }
+        } else {
+          acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
+        }
+        return;
+      case 'rd':
+        if (this.pointBox.t > this.fromPointBox.t) {
+          acc.push({ x: this.connectEnd.x, y: this.fromPointBox.t });
+          acc.push({ x: this.connectStart.x, y: this.fromPointBox.t });
+        } else {
+          if (this.pointBox.r > this.connectEnd.x) {
             acc.push({ x: this.pointBox.r, y: this.connectEnd.y });
             acc.push({ x: this.pointBox.r, y: this.connectStart.y });
           } else {
             acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
           }
-        } else {
-          acc.push({ x: this.connectEnd.x, y: this.fromPointBox.b });
-          acc.push({ x: this.middle.x, y: this.fromPointBox.b });
-          acc.push({ x: this.middle.x, y: this.connectStart.y });
         }
         return;
-      }
-      case 'd': {
-        acc.push({ x: this.connectEnd.x, y: this.middle.y });
-        acc.push({ x: this.connectStart.x, y: this.middle.y });
+      case 'd':
+        acc.push({ x: this.pointBox.r, y: this.connectEnd.y });
+        acc.push({ x: this.pointBox.r, y: this.connectStart.y });
         return;
-      }
     }
   }
 
@@ -216,66 +196,50 @@ export class TopPoint extends Point {
 
     const direction = this._getDirection();
     switch (direction) {
+      case 'r':
       case 'l':
-      case 'ld': {
-        if (this.pointBox.b > this.fromPointBox.t) {
-          if (this.pointBox.l < this.connectEnd.x) {
-            acc.push({ x: this.pointBox.l, y: this.connectEnd.y });
-            acc.push({ x: this.pointBox.l, y: this.pointBox.t });
-            acc.push({ x: this.pointBox.r, y: this.pointBox.t });
-          }
-          acc.push({ x: this.connectEnd.x, y: this.pointBox.t });
-          acc.push({ x: this.connectStart.x, y: this.pointBox.t });
+        acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
+        return;
+      case 'ld':
+        if (this.pointBox.l < this.connectEnd.x) {
+          acc.push({ x: this.pointBox.l, y: this.connectEnd.y });
+          acc.push({ x: this.pointBox.l, y: this.connectStart.y });
         } else {
-          acc.push({ x: this.connectEnd.x, y: this.connectEnd.y });
+          acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
+        }
+        return;
+      case 'd':
+        acc.push({ x: this.pointBox.l, y: this.connectEnd.y });
+        acc.push({ x: this.pointBox.l, y: this.connectStart.y });
+        return;
+      case 'rd':
+        if (this.pointBox.r > this.connectEnd.x) {
+          acc.push({ x: this.pointBox.r, y: this.connectEnd.y });
+          acc.push({ x: this.pointBox.r, y: this.connectStart.y });
+        } else {
+          acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
+        }
+        return;
+      case 'lu':
+        if (this.connectStart.x < this.fromPointBox.r) {
+          acc.push({ x: this.fromPointBox.r, y: this.connectEnd.y });
+          acc.push({ x: this.fromPointBox.r, y: this.connectStart.y });
+        } else {
           acc.push({ x: this.connectStart.x, y: this.connectEnd.y });
         }
         return;
-      }
       case 'u':
-      case 'lu': {
-        if (this.pointBox.r < this.fromPointBox.r) {
-          acc.push({ x: this.fromPointBox.r, y: this.fromPointBox.t });
-          acc.push({ x: this.fromPointBox.r, y: this.connectStart.y });
+        acc.push({ x: this.fromPointBox.r, y: this.connectEnd.y });
+        acc.push({ x: this.fromPointBox.r, y: this.connectStart.y });
+        return;
+      case 'ru':
+        if (this.connectStart.x > this.fromPointBox.l) {
+          acc.push({ x: this.fromPointBox.l, y: this.connectEnd.y });
+          acc.push({ x: this.fromPointBox.l, y: this.connectStart.y });
         } else {
-          if (this.pointBox.t < this.connectEnd.y) {
-            acc.push({ x: this.connectEnd.x, y: this.pointBox.t });
-            acc.push({ x: this.pointBox.r, y: this.pointBox.t });
-            acc.push({ x: this.pointBox.r, y: this.connectStart.y });
-          } else {
-            acc.push({ x: this.connectEnd.x, y: this.fromPointBox.t });
-            acc.push({ x: this.pointBox.r, y: this.fromPointBox.t });
-            acc.push({ x: this.pointBox.r, y: this.connectStart.y });
-          }
+          acc.push({ x: this.connectStart.x, y: this.connectEnd.y });
         }
         return;
-      }
-      case 'r':
-      case 'rd': {
-        acc.push({ x: this.connectEnd.x, y: this.connectStart.y });
-        return;
-      }
-      case 'ru': {
-        if (this.pointBox.r > this.fromPointBox.l) {
-          if (this.connectStart.y < this.fromPointBox.b) {
-            acc.push({ x: this.fromPointBox.r, y: this.fromPointBox.t });
-            acc.push({ x: this.fromPointBox.r, y: this.fromPointBox.b });
-            acc.push({ x: this.connectStart.x, y: this.fromPointBox.b });
-          } else {
-            acc.push({ x: this.fromPointBox.r, y: this.fromPointBox.t });
-            acc.push({ x: this.fromPointBox.r, y: this.connectStart.y });
-          }
-        } else {
-          acc.push({ x: this.middle.x, y: this.connectEnd.y });
-          acc.push({ x: this.middle.x, y: this.connectStart.y });
-        }
-        return;
-      }
-      case 'd': {
-        acc.push({ x: this.connectEnd.x, y: this.middle.y });
-        acc.push({ x: this.connectStart.x, y: this.middle.y });
-        return;
-      }
     }
   }
 }
